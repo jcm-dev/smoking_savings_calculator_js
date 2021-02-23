5410936f40ee8dc142b41435289dca195b433e0e
Joshua <meyer.josh8@gmail.com>
2021-02-23 15:37:24 -0600

    Fix user entry parsing issue and test results as finite

--- app.js
+++ app.js
@@ -18,12 +18,25 @@ function calculateResults(){
   const UItotalYearly = document.getElementById('total-yearly');
   const UItotalCustom = document.getElementById('total-custom');
 
+  const parsePrice = parseFloat(UIprice.value);
+  const parseDaily = parseFloat(UIdaily.value);
+  const parseYears = parseFloat(UIcustYears.value);
   // calculate for each UIouput variable
-  const packPerYear = (UIdaily * 365) / 20;
-  UItotalDaily = UIprice / 20;
-  UItotalMonthly = packPerYear / 12;
-  UItotalYearly = packPerYear * UIprice;
-  UItotalCustom = (packPerYear * UIprice) * UIcustYears;
+  const packPerYear = (parseDaily * 365) / 20;
+
+  if(isFinite(packPerYear)){
+  UItotalDaily.value = (parsePrice / 20).toFixed(2);
+  UItotalMonthly.value = (packPerYear / 12).toFixed(2);
+  UItotalYearly.value = (packPerYear * parsePrice).toFixed(2);
+  UItotalCustom.value = ((packPerYear * parsePrice) * parseYears).toFixed(2);
+
+  // display results
+  document.getElementById('results').style.display = 'block';
+
+  }else{
+    // showError('Please check entry');
+    console.log('asd');
+  }
 }
 
 // DISPLAY ERROR
