c00f31b6aa2a059a85e0c61787b40a45e902c8fb
Joshua <meyer.josh8@gmail.com>
2021-02-23 14:24:52 -0600

    Declare and assign UIinput variables

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ document.getElementById('savings-form').addEventListener('submit', function(e){
 
 // CALCULATE RESULTS
 function calculateResults(){
-  
+  // UIinput variables
+  const UIprice = document.getElementById('price');
+  const UIdaily = document.getElementById('daily');
+  const UIcustYears = document.getElementById('years');
+
+  // UIoutput variables
 }
 
 // DISPLAY ERROR
