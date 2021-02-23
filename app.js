d410d558e7c9ef558dcabd447b11705b1848b34d
Joshua <meyer.josh8@gmail.com>
2021-02-23 14:21:11 -0600

    Create form submit even listener and begin calculateResults function

---
+++ app.js
@@ -0,0 +1,16 @@
+// SUBMIT LISTENER
+document.getElementById('savings-form').addEventListener('submit', function(e){
+
+  calculateResults();
+
+  e.preventDefault();
+});
+
+// CALCULATE RESULTS
+function calculateResults(){
+  
+}
+
+// DISPLAY ERROR
+
+// CLEAR ERROR
