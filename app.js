593a269d97119a9dc331ecf1a9ad1c0b04839d0e
Joshua <meyer.josh8@gmail.com>
2021-02-23 16:28:30 -0600

    Add NaN validation to user input

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // SUBMIT LISTENER
 document.getElementById('savings-form').addEventListener('submit', function(e){
 
-  calculateResults();
+  // hide results and display loading
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 2000);
 
   e.preventDefault();
 });
@@ -18,9 +21,14 @@ function calculateResults(){
   const UItotalYearly = document.getElementById('total-yearly');
   const UItotalCustom = document.getElementById('total-custom');
   // parse text inputs into float
+  if ((!isNaN(UIprice.value))
+  && (!isNaN(UIdaily.value))
+  && (!isNaN(UIcustYears.value))){
   const parsePrice = parseFloat(UIprice.value);
   const parseDaily = parseFloat(UIdaily.value);
   const parseYears = parseFloat(UIcustYears.value);
+  
+
   // calculate for each UIouput variable
   const packPerYear = (parseDaily * 365) / 20;
   // test input for acceptable value
@@ -30,9 +38,10 @@ function calculateResults(){
   UItotalMonthly.value = (packPerYear / 12).toFixed(2);
   UItotalYearly.value = (packPerYear * parsePrice).toFixed(2);
   UItotalCustom.value = ((packPerYear * parsePrice) * parseYears).toFixed(2);
-
-  // display results
+  }
+  // display results and hide loading
   document.getElementById('results').style.display = 'block';
+  document.getElementById('loading').style.display = 'none';
 
   }else{
     showError('Please check entry fields.');
@@ -56,6 +65,7 @@ function showError(error){
   UIerrorDiv.appendChild(document.createTextNode(error));
   // insert UIerrorDiv
   UIcard.insertBefore(UIerrorDiv, UIheading);
+  document.getElementById('loading').style.display = 'none';
   // timeout and clear error after 3 seconds
   setTimeout(clearError, 3000);
 
